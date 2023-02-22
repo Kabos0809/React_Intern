28f07d8343cd6a13ad15fbff8af2599718bb3122
Kabos0809 <hiyamazu70@gmail.com>
2023-02-22 16:28:38 +0900

    importを絶対パスから相対パスに変更

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Route, BrowserRouter } from 'react-router-dom'
-import Login from 'Login'
-import App from 'App'
-
-;<BrowserRouter>
-  <div>
-    <Route path="/">
-      <App />
-    </Route>
-    <Route path="/login">
-      <Login />
-    </Route>
-  </div>
-</BrowserRouter>
+import Login from './Login'
+import App from './App'
