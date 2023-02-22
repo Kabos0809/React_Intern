54328a721992857dfda0e7bcec7b2842bd1f53c8
Kabos0809 <hiyamazu70@gmail.com>
2023-02-22 17:38:01 +0900

    #14 routes/index.tsx src/index.tsx修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'index.css'
-import { RouterConf } from 'routes/index'
+import RouterConf from 'routes/index'
 
 ReactDOM.render(
   <React.StrictMode>
