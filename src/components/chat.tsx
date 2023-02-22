e42c827d15b46a61bdd5e810e87b81d6db7005ae
Kabos0809 <hiyamazu70@gmail.com>
2023-02-22 17:54:02 +0900

    #18 components/chat.tsx作成

---
+++ src/components/chat.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import { Button, FormControl, TextField } from '@mui/material'
+
+const Chat: React.FC = () => {
+  const [chatMsg, setChatMsg] = useState('')
+
+  return <div className="chat"></div>
+}
+
+export default Chat
