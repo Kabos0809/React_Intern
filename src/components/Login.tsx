c35a9af29f1164525dce3abe746ff060e3bd46d8
Kabos0809 <hiyamazu70@gmail.com>
2023-02-26 18:07:48 +0900

    components/Login.tsx #31 ChakraUIに書き換え

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,6 +5,7 @@ import {
   InputRightElement,
   Button,
   VStack,
+  Stack,
 } from '@chakra-ui/react'
 import { ViewIcon } from '@chakra-ui/icons'
 import { auth } from 'config/firebaseConfig'
@@ -23,67 +24,67 @@ const Login: React.FC = (props: any) => {
   }, [props.history])
 
   return (
-    <div>
-      <VStack>
-        <Input
-          name="email"
-          value={email}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setEmail(e.target.value)
-          }
-          placeholder="Email"
-        />
-        <InputGroup>
+    <div className="login">
+      <Stack>
+        <VStack>
           <Input
-            name="pw"
-            value={pw}
+            name="email"
+            value={email}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setPW(e.target.value)
+              setEmail(e.target.value)
             }
-            placeholder="PassWord"
+            placeholder="Email"
           />
-          <InputRightElement>
-            <Button rightIcon={<ViewIcon />} onClick={() => setShow(!isShow)}>
-              Show
-            </Button>
-          </InputRightElement>
-        </InputGroup>
+          <InputGroup>
+            <Input
+              name="pw"
+              value={pw}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPW(e.target.value)
+              }
+              placeholder="PassWord"
+            />
+            <InputRightElement>
+              <Button rightIcon={<ViewIcon />} onClick={() => setShow(!isShow)}>
+                Show
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            onClick={
+              isLogin
+                ? async () => {
+                    try {
+                      await auth.signInWithEmailAndPassword(email, pw)
+                      props.history.push('/')
+                    } catch (e) {
+                      console.log(e)
+                    }
+                  }
+                : async () => {
+                    try {
+                      await auth.createUserWithEmailAndPassword(email, pw)
+                      props.history.push('/')
+                    } catch (e) {
+                      console.log(e)
+                    }
+                  }
+            }
+          >
+            {isLogin ? 'Login' : 'SignUp'}
+          </Button>
+        </VStack>
         <Button
-          variant="contained"
-          color="primary"
-          size="small"
-          onClick={
-            isLogin
-              ? async () => {
-                  try {
-                    await auth.signInWithEmailAndPassword(email, pw)
-                    props.history.push('/')
-                  } catch (e) {
-                    console.log(e)
-                  }
-                }
-              : async () => {
-                  try {
-                    await auth.createUserWithEmailAndPassword(email, pw)
-                    props.history.push('/')
-                  } catch (e) {
-                    console.log(e)
-                  }
-                }
-          }
-        >
-          {isLogin ? 'Login' : 'SignUp'}
-        </Button>
-      </VStack>
-      <Typography>
-        <button
           onClick={() => {
             setIsLogin(!isLogin)
           }}
         >
           {isLogin ? 'SignUP' : 'Login'}
-        </button>
-      </Typography>
+        </Button>
+      </Stack>
     </div>
   )
 }
